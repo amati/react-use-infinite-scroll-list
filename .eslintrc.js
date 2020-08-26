deefef8d568dce7032616b394a9462457b51d0ad
chanhyun.roh <chanhyun.roh@linecorp.com>
2020-08-27 00:26:22 +0900

    project init

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: ['amati', 'amati/react'],
+  // optional override
+  'no-unused-expressions': ['error', { allowShortCircuit: true }],
+};
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'useToggle',
+    libraryTarget: 'umd',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|dist)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  externals: {
+    react: 'react',
+  },
+};
