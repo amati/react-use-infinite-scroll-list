24f69fddf03fbe26ce78830de352de1b77082732
Chanhyun, Roh <preserde@gmail.com>
2020-09-02 16:22:26 +0900

    Feature/init (#1)
    * project init
    
    * add source and build
    
    * update env
    
    * main file
    
    * version update
    
    Co-authored-by: chanhyun.roh <chanhyun.roh@linecorp.com>

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import useInfiniteScrollList from './use-infinite-scroll-list';
+export default useInfiniteScrollList;
---
+++ src/use-infinite-scroll-list.js
@@ -0,0 +1,133 @@
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import 'intersection-observer';
+
+const defaultLoadingElement = (
+  <div
+    style={{
+      display: 'flex',
+      width: '100%',
+      padding: '15px 0 0',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30">
+      <style>{`
+        .loading_spinner circle {
+          transform-origin: 15px 15px;
+          animation: animationStroke 1s linear infinite, animationTransform 1s linear infinite;
+        }
+        @keyframes animationStroke {
+          0%,
+          100% {
+            stroke-dasharray: 0, 80;
+          }
+          50% {
+            stroke-dasharray: 70, 80;
+          }
+        }
+        @keyframes animationTransform {
+          0% {
+            transform: rotate(0);
+          }
+          50% {
+            transform: rotate(320deg);
+          }
+          100% {
+            transform: rotate(720deg);
+          }
+        }
+        `}</style>
+      <circle
+        cx="15"
+        cy="15"
+        fill="none"
+        r="13"
+        stroke="#b7b7b7"
+        strokeWidth="2"
+        strokeLinecap="round"
+        transform="rotate(216.57 15 15)">
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          calcMode="linear"
+          values="0 15 15;320 15 15;720 15 15"
+          keyTimes="0;0.5;1"
+          dur="1s"
+          begin="0s"
+          repeatCount="indefinite"></animateTransform>
+        <animate
+          attributeName="stroke-dasharray"
+          calcMode="linear"
+          values="0 80; 70 80; 00 80"
+          keyTimes="0;0.5;1"
+          dur="1"
+          begin="0s"
+          repeatCount="indefinite"></animate>
+      </circle>
+    </svg>
+  </div>
+);
+
+export default ({ init, more, loadingElement = defaultLoadingElement }) => {
+  const [endOfList, setEndOfList] = useState(null);
+  const [isFetching, setIsFetching] = useState(false);
+  const [isDone, setIsDone] = useState(false);
+
+  const observer = useRef();
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting && !isFetching) {
+            setIsFetching(true);
+          }
+        });
+      },
+      {
+        threshold: 1,
+        rootMargin: '0px 0px 50px',
+      },
+    );
+
+    async function fetchInit() {
+      const result = await init();
+      setIsDone(result.isDone);
+    }
+    fetchInit();
+  }, []);
+
+  useEffect(() => {
+    const currentObserver = observer.current;
+    endOfList && currentObserver.observe(endOfList);
+    return () => {
+      endOfList && currentObserver.unobserve(endOfList);
+    };
+  }, [endOfList]);
+
+  useEffect(() => {
+    async function fetchMore() {
+      const result = await more();
+      setIsDone(result.isDone);
+      setIsFetching(false);
+    }
+    if (isFetching && !isDone) {
+      fetchMore();
+    }
+  }, [isFetching, isDone]);
+
+  const InfiniteScrollList = useCallback(
+    ({ children }) => {
+      return (
+        <>
+          {children}
+          <div id="__INFINITE_SCROLL_BOTTOM__" ref={setEndOfList} />
+          {isFetching && !isDone && loadingElement}
+        </>
+      );
+    },
+    [isDone, isFetching],
+  );
+
+  return { InfiniteScrollList };
+};
